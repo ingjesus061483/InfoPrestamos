95999aece550ad0bad65d004e1dba039130900a9
ingjesus061483 <51796225+ingjesus061483@users.noreply.github.com>
2025-10-03 23:36:04 -0500

    prueba sms

---
+++ EasyCredit/Models/Sms.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+using System.Configuration;
+public class Sms
+{  
+    static string AccountSid { get { return ConfigurationManager.AppSettings["AutenticacionID"].ToString(); } }
+    static string AuthToken { get { return ConfigurationManager.AppSettings["AutenticacionToken"].ToString(); } }
+    static string From { get { return ConfigurationManager.AppSettings["From"].ToString(); } }
+    public  static void Main(string telefono ,string body)
+    {
+        TwilioClient.Init(AccountSid, AuthToken);
+        var messageOptions = new CreateMessageOptions(
+          new PhoneNumber($"+57{telefono}"));
+        messageOptions.From = new PhoneNumber(From);//PhoneNumber("+12566496782")
+        messageOptions.Body = body;
+        var message = MessageResource.Create(messageOptions);
+        Console.WriteLine(message.Body);
+    }
+}
